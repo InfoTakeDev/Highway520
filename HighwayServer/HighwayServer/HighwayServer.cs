0efa625663eded5eefab2b1a078a7886ebfedbdf
musiceater <musiceater.hsiao@gmail.com>
2013-06-28 01:13:29 +0800

    first commit

---
+++ HighwayServer/HighwayServer/HighwayServer.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Collections; // for ArrayList
+
+namespace Highway520
+{
+    public struct GeneralInfo
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }// ID equals to position
+    }
+
+    public struct SpeedInfo
+    {
+        public string NS_EW { get; set; }
+        public string SN_WE { get; set; }
+    }
+
+    public struct NodeInfo
+    {
+        //public string from { get; set; }
+        //public string to { get; set; }
+        public string Name { get; set; }
+        public SpeedInfo Speed;
+        public string Incident { get; set; }
+        // 1,2 for South to North/South to North, 3,4 for East to West/East to West
+        public int Direction { get; set; }
+    }
+
+    public class Section
+    {
+        private string HWID;
+        private string StartID;
+        private string EndID;
+        public Section(string hwid, string startid, string endid)
+        {
+            this.HWID = hwid;
+            if (Convert.ToInt32(endid) < Convert.ToInt32(startid))
+            {
+                this.StartID = endid;
+                this.EndID = startid;
+            }
+            else
+            {
+                this.StartID = startid;
+                this.EndID = endid;
+            }
+
+        }
+        public ArrayList getTrafficNodeList()
+        {
+            string querytrafficlist = HighwayServer.BaseURI + "/traffic/getsectraffic/fid/" + this.HWID + "/from/" + StartID + "/end/" + EndID;
+            Tool tl = new Tool();
+            string sbstr = tl.queryInfo(querytrafficlist);
+            ArrayList nodelist = tl.parseSpeed(sbstr);
+            return nodelist;
+        }
+    }
+
+    public class Highway
+    {
+        private string HighwayID;
+        public Highway(string hwid)
+        {
+            this.HighwayID = hwid;
+        }
+
+        // getSectoinList
+        public ArrayList getSectionList()
+        {
+            string queryseclist = HighwayServer.BaseURI + "/common/getnodsecs/fid/" + this.HighwayID + "?lc=1&id=end_selt";
+            Tool tl = new Tool();
+            string sbstr = tl.queryInfo(queryseclist);
+            ArrayList seclist = tl.ParseInfo(sbstr);
+            return seclist;
+        }
+        // getSectionInformation returns traffic information
+        public Section getSectionObject(GeneralInfo startsec, GeneralInfo endsec)
+        {
+            Section sec = new Section(this.HighwayID, startsec.ID, endsec.ID);
+            return sec;
+        }
+
+    }
+
+    public class HighwayServer
+    {
+        public const string BaseURI = "http://1968.freeway.gov.tw";
+        public HighwayServer()
+        {
+        }
+
+        public ArrayList getHighwayList()
+        {
+            string queryhwlist = BaseURI + "/common/getfrees?id=sec_selt&df=1";
+            Tool tl = new Tool();
+            string sbstr = tl.queryInfo(queryhwlist);
+            ArrayList hwlist = tl.ParseInfo(sbstr);
+            return hwlist;
+        }
+        // return a highway object
+        public Highway getHighwayObject(GeneralInfo highway)
+        {
+            Highway hw = new Highway(highway.ID);
+            return hw;
+        }
+
+    }
+
+    class Tool
+    {
+
+
+        public string queryInfo(string URI)
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create(URI);
+            System.Net.WebResponse resp = req.GetResponse();
+            System.IO.StreamReader sr = new System.IO.StreamReader(resp.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+
+        public ArrayList ParseInfo(string dom)
+        {
+            XDocument doc = XDocument.Parse(dom);
+            ArrayList infoList = new ArrayList();
+            foreach (XElement el in doc.Root.Elements())
+            {
+                GeneralInfo gi = new GeneralInfo();
+                gi.Name = el.Value;
+                gi.ID = el.Attribute("value").Value;
+                infoList.Add(gi);
+            }
+            return infoList;
+        }
+
+        public ArrayList parseSpeed(string dom)
+        {
+            int idx = dom.IndexOf("<script type=");
+            int direction = 1;
+            if (idx != -1)
+            {
+                /*
+			    1,2 for North/South
+			    3,4 for West/East
+		        */
+                Console.WriteLine(dom);
+                foreach (string line in dom.Substring(idx).Split('\n'))
+                {
+
+                    if (line.IndexOf("direction =") != -1)
+                    {
+                        Console.WriteLine(line);
+                        direction = Convert.ToInt16(line.Substring(11 + 1).TrimEnd(';'));
+                        Console.WriteLine("dir=" + direction);
+                    }
+                }
+                dom = "<root>" + dom.Substring(0, idx) + "</root>";
+            }
+
+            XDocument doc = XDocument.Parse(dom);
+            ArrayList NodeList = new ArrayList();
+
+            foreach (XElement childtr in doc.Root.Elements())
+            {
+                NodeInfo ni = new NodeInfo();
+                ni.Direction = direction;
+                foreach (XElement childtd in childtr.Elements())
+                {
+                    switch (childtd.Attribute("class").Value)
+                    {
+                        case "speed speedLeft":
+                            ni.Speed.SN_WE = childtd.Value.Trim();
+                            break;
+                        case "sec_name":
+                            // parse the information
+                            ni.Name = childtd.Value.Trim(); ;
+                            break;
+                        case "speed speedRight":
+                            ni.Speed.NS_EW = childtd.Value.Trim(); ;
+                            break;
+                    }
+                }
+                //Console.WriteLine("{0} {1} {2} {3}", ni.direction, ni.speed.SN_WE, ni.speed.NS_EW, ni.name);
+                NodeList.Add(ni);
+            }
+            return NodeList;
+        }
+
+    }
+}
---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Highway520;
+using System.Collections;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HighwayServer hwserver;
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            hwserver = new HighwayServer();
+            ArrayList hwlist = hwserver.getHighwayList();
+            foreach (GeneralInfo item in hwlist)
+            {
+                listBox_freeway.Items.Add(item.ID + "-" + item.Name);
+                //listBox_freeway.Items.Add(item.ID);
+                
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string str = listBox_freeway.SelectedItem.ToString();
+            string []tmpstr = str.Split('-');
+            GeneralInfo tmphw = new GeneralInfo();
+            tmphw.ID=tmpstr[0];
+            tmphw.Name=tmpstr[1];
+            Highway hwobj = hwserver.getHighwayObject(tmphw);
+            ArrayList seclist = hwobj.getSectionList();
+            foreach (GeneralInfo item in seclist)
+            {
+                listBox_section.Items.Add(item.ID + "-" + item.Name);
+                // .Show(item.ID + " " + item.Name);
+            }
+            Highway520.Section secobj = hwobj.getSectionObject((GeneralInfo)seclist[0], (GeneralInfo)seclist[seclist.Count-1]);
+            ArrayList nodelist = secobj.getTrafficNodeList();
+
+            foreach (NodeInfo item in nodelist)
+            {
+                listBox_traffic
+.Items.Add(item.Direction + " " + item.Speed.SN_WE + " " + item.Speed.NS_EW + " " + item.Name);
+            }
+            
+            
+        }
+    }
+}
